8c1ac3588bb95c04a6e9fae31d36d3de11c0b7e6
Rob Scharp <rscharpf@jhsph.edu>
2007-08-11 20:44:33 +0000

    provided coercion methods for deprecated classes
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/SNPchip@26330 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -1,12 +1,47 @@
-##setClass("SnpLayout", representation(mat="matrix",
-##                                     widths="numeric",
-##                                     heights="numeric",
-##                                     respect="logical"))
 setClass("ParESet", representation(snpPar="list"))
 setClass("ParSnpCopyNumberSet", contains="ParESet")
 setClass("ParSnpCallSet", contains="ParESet")
 setClass("ParSnpSet", contains="ParSnpCopyNumberSet")
 
-    
+setClass("AnnotatedSnpSet", contains="oligoSnpSet")
+setClass("AnnotatedSnpCallSet", contains="SnpCallSet")
+setClass("AnnotatedSnpCopyNumberSet", contains="SnpCopyNumberSet")
+setAs("AnnotatedSnpSet", "oligoSnpSet",
+      function(from, to){
+        warning("AnnotatedSnpSet DEPRECATED")
+        new("oligoSnpSet",
+            copyNumber=copyNumber(from),
+            cnConfidence=cnConfidence(from),
+            calls=calls(from),
+            callsConfidence=callsConfidence(from),
+            experimentData=experimentData(from),
+            featureData=featureData(from),
+            phenoData=phenoData(from),
+            annotation=annotation(from))
+      })
+
+setAs("AnnotatedSnpCallSet", "SnpCallSet",
+      function(from, to){
+        warning("AnnotatedSnpCallSet DEPRECATED")
+        new("SnpCallSet",
+            calls=calls(from),
+            callsConfidence=callsConfidence(from),
+            experimentData=experimentData(from),
+            featureData=featureData(from),
+            phenoData=phenoData(from),
+            annotation=annotation(from))
+      })
+
+setAs("AnnotatedSnpCopyNumberSet", "SnpCopyNumberSet",
+      function(from, to){
+        warning("AnnotatedSnpCopyNumberSet DEPRECATED")
+        new("SnpCopyNumberSet",
+            copyNumber=copyNumber(from),
+            cnConfidence=cnConfidence(from),
+            experimentData=experimentData(from),
+            featureData=featureData(from),
+            phenoData=phenoData(from),
+            annotation=annotation(from))
+      })
 
 
